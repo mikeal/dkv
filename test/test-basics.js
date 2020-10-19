63c461684a9de8c4c5421b30eb9cb70898c8c954
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-10-19 02:34:17 +0000

    fix: better mock ipfs

--- test/test-basics.js
+++ test/test-basics.js
@@ -1,17 +1,37 @@
 import DKV from '../index.js'
 import { deepStrictEqual as same } from 'assert'
-import IPFS from 'ipfs'
+import ipfsModule from 'ipfs'
+import Ctl from 'ipfsd-ctl'
 
-const opts = { offline: true, start: false, silent: true }
-
-// terrible hack until we can get a better ipfs instance for testing
+/* terrible hack until we can get a better ipfs instance for testing
 setTimeout(() => {
   process.exit()
 }, 1000 * 3)
+*/
+
+const createIPFS = () => Ctl.createController({
+  type: 'proc',
+  ipfsModule,
+  test: true,
+  disposable: true
+})
+
+const setup = async test => {
+  const ipfsd = await createIPFS()
+  test.after(() => ipfsd.stop())
+  return ipfsd.api
+}
 
 export default async test => {
   test('put/get roundtrip', async test => {
-    const ipfs = await IPFS.create(opts)
+    const ipfs = await setup(test)
+    let kv = await DKV.empty(ipfs)
+    kv = await kv.set('test', { hello: 'world' })
+    same(await kv.get('test'), { hello: 'world' })
+    ipfs.stop()
+  })
+  test('put/get roundtrip', async test => {
+    const ipfs = await setup(test)
     let kv = await DKV.empty(ipfs)
     kv = await kv.set('test', { hello: 'world' })
     same(await kv.get('test'), { hello: 'world' })
